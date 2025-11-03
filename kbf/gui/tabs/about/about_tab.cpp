00131d43874a21d3445cc8238658b05a1e5ea431
kana-desu <kana.no.kanae.ten@gmail.com>
2025-11-03 17:04:37 +0000

    Bump version to v1.0.4

--- kbf/gui/tabs/about/about_tab.cpp
+++ kbf/gui/tabs/about/about_tab.cpp
@@ -632,7 +632,18 @@ namespace kbf {
     void AboutTab::drawChangelogTab() {
 
         CImGui::Spacing();
-        if (CImGui::CollapsingHeader("v1.0.3b", ImGuiTreeNodeFlags_DefaultOpen)) {
+        if (CImGui::CollapsingHeader("v1.0.4b", ImGuiTreeNodeFlags_DefaultOpen)) {
+            CImGui::Spacing();
+            CImGui::SeparatorText("Additions");
+            WRAP_BULLET("-", "Persistent files (e.g. presets, etc) will now be upgraded to the latest format automatically when there is an update.");
+            WRAP_BULLET("-", "Added the option to always show a part, as well as hide it.");
+            CImGui::SeparatorText("Changes");
+            WRAP_BULLET("-", "Downgraded some KBF console errors to warnings.");
+            CImGui::SeparatorText("Fixes");
+            WRAP_BULLET("-", "Fixed presets always causing all parts to show up, even if they are supposed to be disabled.");
+            CImGui::Spacing();
+        }
+        if (CImGui::CollapsingHeader("v1.0.3b")) {
             CImGui::Spacing();
             CImGui::SeparatorText("Additions");
             WRAP_BULLET("-", "Added links to visual tutorials on GitHub.");
