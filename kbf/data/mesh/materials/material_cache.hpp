d8126c943d8e365579726f575fa54fc8514e41c5
kana-desu <kana.no.kanae.ten@gmail.com>
2025-11-17 02:21:26 +0000

    Material cache helpers for piecewise access

--- kbf/data/mesh/materials/material_cache.hpp
+++ kbf/data/mesh/materials/material_cache.hpp
@@ -14,6 +14,33 @@ namespace kbf {
 		HashedMaterialList arms;
 		HashedMaterialList coil;
 		HashedMaterialList legs;
+
+		HashedMaterialList& getPieceCache(ArmourPiece piece) {
+			switch (piece)
+			{
+			case ArmourPiece::AP_HELM: return helm;
+			case ArmourPiece::AP_BODY: return body;
+			case ArmourPiece::AP_ARMS: return arms;
+			case ArmourPiece::AP_COIL: return coil;
+			case ArmourPiece::AP_LEGS: return legs;
+			}
+
+			throw std::runtime_error("[MaterialCache] Invalid cache request - piece not supported.");
+		}
+
+		const HashedMaterialList& getPieceCache(ArmourPiece piece) const {
+			switch (piece)
+			{
+			case ArmourPiece::AP_HELM: return helm;
+			case ArmourPiece::AP_BODY: return body;
+			case ArmourPiece::AP_ARMS: return arms;
+			case ArmourPiece::AP_COIL: return coil;
+			case ArmourPiece::AP_LEGS: return legs;
+			}
+
+			throw std::runtime_error("[MaterialCache] Invalid cache request - piece not supported.");
+		}
+
 	};
 
 }
