b6f64e1b79fa24c493162166f028096b3a0e729a
kana-desu <kana.no.kanae.ten@gmail.com>
2025-11-21 15:09:25 +0000

    Add post build step to generate .pdb at dist/out/pdb for dist builds

--- debug/resolve_symbols_example.bat
+++ debug/resolve_symbols_example.bat
@@ -1,4 +1,4 @@
-python resolve_symbols.py "C:\SteamLibrary\steamapps\common\MonsterHunterWilds\reframework\data\KBF\KanaBodyFramework_logic.dll.pdb" ^
+python resolve_symbols.py "../dist/out/pdb/KanasBodyFramework_v1.0.7.pdb" ^
 --function-rvas ^
 0x00000000001f5674 ^
 0x000000000021bfb2 ^
